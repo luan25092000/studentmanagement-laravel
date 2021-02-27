29167f4c2a8643581a82922c0f57675f3e19beed
Nguyễn Hữu Luân <nguyenhuuluan17@gmail.com>
2021-02-27 09:59:43 +0700

    Add modal confirmation dialog on delete

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title')</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="shortcut icon" type="image/png" href="{{ URL::to('/') }}/favicon.ico" />
@@ -75,5 +76,6 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -12,8 +12,33 @@
             <a href="{{ route('index') }}" type="button" class="btn btn-secondary">Back</a>
             @role('admin') 
             <a href="{{ route('edit.student.form',['id' => $student['id']]) }}" type="button" class="btn btn-warning">Edit</a>  
-            <a href="{{ route('delete.student',['id' => $student['id']]) }}" type="button" class="btn btn-danger">Delete</a>
+            <a data-href="{{ route('delete.student',['id' => $student['id']]) }}" data-toggle="modal" data-target="#confirm-delete" type="button" class="btn btn-danger">Delete</a>
             @endrole 
+        </div>
+    </div>
+    <!-- The Modal -->
+    <div class="modal" id="confirm-delete">
+        <div class="modal-dialog">
+            <div class="modal-content">
+        
+                <!-- Modal Header -->
+                <div class="modal-header">
+                    <h4 class="modal-title">Message</h4>
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                </div>
+        
+                <!-- Modal body -->
+                <div class="modal-body">
+                    Do you want to delete "{{ $student['firstname'] }} {{ $student['lastname'] }}" student ?
+                </div>
+        
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <a class="btn btn-primary btn-ok">Yes</a>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+        
+            </div>
         </div>
     </div>
 @endsection
